a51eeb8a46ab7c5e549159a7255d61b7564b8f67
SolAlak <salak@udesa.edu.ar>
2025-04-13 10:53:20 -0300

    cambios en login, register, reemplazo de archivos

---
+++ myapp/controllers/userController.js
@@ -0,0 +1,41 @@
+const db = require('../db/usuarios');
+
+const userController = {
+    login: function (req, res) {
+        return res.render('login');
+    },
+
+    perfil: function (req, res) {
+        const emailBuscado = req.params.email;
+        const listaUsuarios = db.usuarios;
+
+        let usuarioEncontrado = null;
+
+        for (let i = 0; i < listaUsuarios.length; i++) {
+            const usuario = listaUsuarios[i];
+            if (usuario.email === emailBuscado) {
+                usuarioEncontrado = usuario;
+            }
+        }
+
+        if (usuarioEncontrado) {
+            const nombre = usuarioEncontrado.nombre;
+            const ft = usuarioEncontrado.fotoPerfil;
+            const mail = usuarioEncontrado.email;
+
+            return res.render('profile', { nombre, ft, mail });
+        } else {
+            return res.send('No encontramos al usuario con ese email');
+        }
+    },
+
+    register: function (req, res) {
+        return res.render('register');
+    }
+};
+
+
+module.exports = userController;
+
+
+
