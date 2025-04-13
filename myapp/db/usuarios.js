9dfe4ea4e7752087d8fd463f725a6c93a1d3e294
KailaniFarkas <kfarkas@udesa.edu.ar>
2025-04-13 09:45:25 -0300

    usuarios db listaUsuarios

---
+++ myapp/db/usuarios.js
@@ -0,0 +1,96 @@
+const listaUsuarios = {
+    usuarios: [
+        {
+            id: 1,
+            nombre: "Juan Pérez",
+            email: "juanperez@gmail.com",
+            contrasenia: "clave123",
+            dni: "30123456",
+            fechaNacimiento: "15-04-1990",
+            fotoPerfil: "juan.jpg"
+        },
+        {
+            id: 2,
+            nombre: "Ana Gómez",
+            email: "anagomez@gmail.com",
+            contrasenia: "pass456",
+            dni: "31234567",
+            fechaNacimiento: "22-11-1988",
+            fotoPerfil: "ana.png"
+        },
+        {
+            id: 3,
+            nombre: "Carlos Díaz",
+            email: "carlosdiaz@gmail.com",
+            contrasenia: "123carlos",
+            dni: "28765432",
+            fechaNacimiento: "09-06-1995",
+            fotoPerfil: "carlos.jpg"
+        },
+        {
+            id: 4,
+            nombre: "Lucía Fernández",
+            email: "luciaf@gmail.com",
+            contrasenia: "lucia2023",
+            dni: "29543210",
+            fechaNacimiento: "01-03-1992",
+            fotoPerfil: "lucia.jpeg"
+        },
+        {
+            id: 5,
+            nombre: "Martín López",
+            email: "martinlopez@gmail.com",
+            contrasenia: "ml1234",
+            dni: "30345678",
+            fechaNacimiento: "12-08-1985",
+            fotoPerfil: "martin.png"
+        },
+        {
+            id: 6,
+            nombre: "Florencia Ruiz",
+            email: "florruiz@gmail.com",
+            contrasenia: "ruizflor",
+            dni: "31112222",
+            fechaNacimiento: "25-10-1993",
+            fotoPerfil: "flor.jpg"
+        },
+        {
+            id: 7,
+            nombre: "Diego Torres",
+            email: "diegotorres@gmail.com",
+            contrasenia: "torres456",
+            dni: "29998888",
+            fechaNacimiento: "19-07-1991",
+            fotoPerfil: "diego.png"
+        },
+        {
+            id: 8,
+            nombre: "María Silva",
+            email: "mariasilva@gmail.com",
+            contrasenia: "silva789",
+            dni: "29887766",
+            fechaNacimiento: "30-12-1996",
+            fotoPerfil: "maria.jpeg"
+        },
+        {
+            id: 9,
+            nombre: "Nicolás Herrera",
+            email: "nico.herrera@gmail.com",
+            contrasenia: "nh123456",
+            dni: "29001122",
+            fechaNacimiento: "05-05-1989",
+            fotoPerfil: "nicolas.jpg"
+        },
+        {
+            id: 10,
+            nombre: "Valentina Castro",
+            email: "valencastro@gmail.com",
+            contrasenia: "castrovalen",
+            dni: "30001234",
+            fechaNacimiento: "14-02-1994",
+            fotoPerfil: "valentina.png"
+        }
+    ]
+}
+
+module.exports = listaUsuarios;
