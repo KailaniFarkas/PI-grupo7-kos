89e20671788b617162fb9bd75185281c1308845a
SolAlak <salak@udesa.edu.ar>
2025-04-13 08:42:01 -0300

    testing

--- myapp/routes/login.js
+++ myapp/routes/login.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-// const loginController = require('../controllers/loginController');
+const loginController = require('../controllers/loginController');
 
-// router.get('/login', loginController);
+router.get('/login/email/:email', loginController);
 
-// router.get('/login', function(req, res) {
- //   const { email, password } = req.query;
+router.get('/login', function(req, res) {
+    const { email, password } = req.body;
     // lógica del controlador
-// });
+});
